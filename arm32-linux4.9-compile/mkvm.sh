e49b9a881dcbe0bae8e01335c0ac98cb02925b72
lucaisacoder <lucas_hsueh@hotmail.com>
2019-03-02 15:23:19 +0800

    update sh

--- arm32-linux4.9-compile/mkvm.sh
+++ arm32-linux4.9-compile/mkvm.sh
@@ -18,7 +18,8 @@ cp linux-4.9/arch/arm/boot/dts/vexpress-v2p-ca9.dtb vm/
 cp linux-4.9/arch/arm/boot/zImage vm/ 
 
 # generate boot.sh mkrootfs.sh 
-cat > vm/boot.sh << EOF 
+cat > vm/boot.sh << EOF
+mkrootfs.sh 
 qemu-system-arm -M vexpress-a9 -m 512M -kernel ./zImage -dtb ./vexpress-v2p-ca9.dtb -nographic -append "root=/dev/mmcblk0  console=ttyAMA0" -sd a9rootfs.ext3 
 EOF
 
